d860607c4a6842047c4f3e1ec3412ad4c58349a8
ogohogo <78665139+ogohogo@users.noreply.github.com>
2022-09-08 19:25:45 +0200

    small code rewrite

--- main.js
+++ main.js
@@ -22,33 +22,38 @@ const waitForElement = (selector) => {
 
 //#endregion
 
-var channelMonetized = `<div class='channelMonetization' style='font-size:13px; margin-top:2px; color: #4CBB17;'>Channel is monetized</div>`;
-var channelNotMonetized = `<div class='channelMonetization' style='font-size:13px; margin-top:2px; color: #D22B2B;'>Channel is not monetized</div>`;
-var currentURL = window.location.href.split("?")[0];
-
-function checkForValidURL(url) {
+//#region Check for valid URL function
+const checkForValidURL = (url) => {
   return url.includes("/channel/") || url.includes("/c/") || url.includes("/user/")
 }
+//#endregion
+
+var channelMonetized = `<div style='font-size:13px; margin-top:2px; color: #4CBB17;'>Channel is monetized</div>`;
+var channelNotMonetized = `<div style='font-size:13px; margin-top:2px; color: #D22B2B;'>Channel is not monetized</div>`;
+var loadingMonetizationStatus = `<div style='font-size:13px; margin-top:2px; color: #FFFF00;'>Loading monetization data...</div>`;
+var failedToLoad = `<div style='font-size:13px; margin-top:2px; color: #D22B2B;'>Failed to gather monetization data! Please report this.</div>`;
+
+var currentURL = window.location.href.split("?")[0].split("#")[0]
 
 function getDataOnFirstLoad() {
+  document.querySelector("#subscriber-count").insertAdjacentHTML('afterEnd', `<div class='channelMonetization'>${loadingMonetizationStatus}</div>`)
+
   let isMonetized = document.documentElement.innerHTML
     .split(`{"key":"is_monetization_enabled","value":"`)[1]
     .split(`"},`)[0];
-  return document.querySelector("#subscriber-count").insertAdjacentHTML('afterEnd', `${isMonetized == 'true' ? channelMonetized : channelNotMonetized}`)
+
+  return document.querySelector(".channelMonetization").innerHTML = isMonetized == 'true' ? channelMonetized : channelNotMonetized
 }
 
 window.onload = function () {
   if (!checkForValidURL(window.location.href)) return;
 
-  waitForElement("#subscriber-count").then((elm) => {
-    console.log("Element is ready");
-    getDataOnFirstLoad();
+  waitForElement("#subscriber-count").then(() => {
+    return getDataOnFirstLoad();
   });
 };
 
 setInterval(() => {
-
-  console.log(window.location.href)
 
   if (currentURL == window.location.href.split("?")[0]) return;
   if (!checkForValidURL(window.location.href)) return;
@@ -56,23 +61,24 @@ setInterval(() => {
 
   try {
 
-    let element = document.querySelector(".channelMonetization")
+    let element = document.querySelector(".channelMonetization");
+
+    if (!element) return document.querySelector("#subscriber-count").insertAdjacentHTML('afterEnd', `<div class='channelMonetization'>${loadingMonetizationStatus}</div>`)
+    else element.innerHTML = loadingMonetizationStatus;
 
     var req = new XMLHttpRequest();
     req.open('GET', currentURL, false);
     req.send(null);
 
-    if (req.status != 200) return element.innerHTML = ``
+    if (req.status != 200) return element.innerHTML = failedToLoad;
 
     let res = req.responseText
     let isMonetized = res.split(`{"key":"is_monetization_enabled","value":"`)[1].split(`"},`)[0]
 
-    if (!element) return document.querySelector("#subscriber-count").insertAdjacentHTML('afterEnd', `${isMonetized == 'true' ? channelMonetized : channelNotMonetized}`)
-
-    return element.innerHTML = `${isMonetized == 'true' ? channelMonetized : channelNotMonetized}`
+    return element.innerHTML = isMonetized == 'true' ? channelMonetized : channelNotMonetized
   } catch (e) {
-    element.innerHTML = ``
+    element.innerHTML = failedToLoad;
     return console.log(`An error occured while attempting to fetch data from YouTube\n${e}`)
   }
 
-}, 1000)
+}, 250)
